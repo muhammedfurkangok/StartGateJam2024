46f6ec0505d7ee1b2112b8b9b975a0d93d2c5143
Özgür Dalbeler <ozgurdalbeler@gmail.com>
2024-12-14 21:34:30 +0300

    Portal Done

---
+++ Assets/Scripts/Portal.cs
@@ -0,0 +1,29 @@
+﻿using Cysharp.Threading.Tasks;
+using UnityEngine;
+
+public class Portal : MonoBehaviour
+{
+    [Header("References")]
+    [SerializeField] private Portal linkedPortal;
+
+    [Header("Info")]
+    [SerializeField] private bool isPlayerInPortal;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player") && !isPlayerInPortal)
+        {
+            linkedPortal.isPlayerInPortal = true;
+            other.transform.position = linkedPortal.transform.position;
+            PlayerTeleported().Forget();
+        }
+    }
+
+    private async UniTask PlayerTeleported()
+    {
+        linkedPortal.isPlayerInPortal = true;
+        await UniTask.WaitForFixedUpdate();
+        await UniTask.WaitForFixedUpdate();
+        linkedPortal.isPlayerInPortal = false;
+    }
+}
